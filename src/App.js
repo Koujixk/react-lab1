5a8563bbc2bba460f59238406794c56c309d08e9
Koujixk <arsiliazov@gmai.com>
2022-08-10 11:02:27 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn google
         </a>
       </header>
     </div>
